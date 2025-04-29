61d8b71b8937f4f252d71eeba89b6f91486bbf81
gcontiero11 <gustcontiero@gmail.com>
2025-04-29 14:46:43 -0300

    fix: adding a new remote bot on the spring execution to defaultUser

--- web/src/main/java/com/bueno/WebApp.java
+++ web/src/main/java/com/bueno/WebApp.java
@@ -67,8 +67,11 @@ public class WebApp {
             final UUID user1Uuid = registerUserUseCase.create(user1).uuid();
             final UUID user2Uuid = registerUserUseCase.create(user2).uuid();
             final TransientRemoteBotDto remoteBot = new TransientRemoteBotDto(UUID.randomUUID(), defaultUuid, "Remote Bot", "http://localhost", "8030", "https://github.com/gcontiero11/CTruco");
+            final TransientRemoteBotDto djangoRemoteBot = new TransientRemoteBotDto(UUID.randomUUID(), defaultUuid, "DjangoRemoteBot", "http://localhost", "8000", "https://github.com/gcontiero11/CTruco");
             botRepository.save(remoteBot);
+            botRepository.save(djangoRemoteBot);
             botRepository.authorizeByUuid(remoteBot.uuid());
+            botRepository.authorizeByUuid(djangoRemoteBot.uuid());
             for (int i = 0; i < 30; i++) {
                 gameResultRepository.save(new GameResultDto(UUID.randomUUID(), LocalDateTime.now().minusMinutes(5),
                         LocalDateTime.now(), defaultUuid, defaultUuid, 12, user1Uuid, 3));
